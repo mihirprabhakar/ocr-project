61f2df6704a58305951c73b0c067252ba4c5919c
mihirprabhakar <mihirprabhakar6@gmail.com>
2026-06-12 00:00:45 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -31,12 +31,15 @@ if (process.env.NODE_ENV === 'production') {
   );
 }
 
-// Connect to MongoDB
+
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB Connected');
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT;
+    // const PORT = process.env.PORT || 5000;
+    // use the PORT from environment variable, default to 5000 if not set .
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => {
